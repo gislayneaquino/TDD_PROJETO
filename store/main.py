e95862dad72173b26ddc45a290d4fa26cec79991
gislayne <gislayneaaquino@gmail.com>
2024-06-13 20:59:01 -0300

    feat: atualizaçoes

--- store/main.py
+++ store/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+
 from store.core.config import settings
+from store.routers import api_router
 
 
 class App(FastAPI):
@@ -9,8 +11,9 @@ class App(FastAPI):
             **kwargs,
             version="0.0.1",
             title=settings.PROJECT_NAME,
-            root_path=settings.ROOTH_PATH
+            root_path=settings.ROOT_PATH
         )
 
 
 app = App()
+app.include_router(api_router)
